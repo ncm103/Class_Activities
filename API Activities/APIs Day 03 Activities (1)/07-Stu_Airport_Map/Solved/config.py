238801f4ec8cc023465a14e6914f4792c91bb50d
ncm103 <ncmeier9794@gmail.com>
2020-02-12 19:45:25 -0500

    Commit

---
+++ API Activities/APIs Day 03 Activities (1)/07-Stu_Airport_Map/Solved/config.py
@@ -0,0 +1,2 @@
+# Enter your API key
+gkey = "AIzaSyCu1ZA_-wimdbPaUssBz2Y3IjJQ_3FEaGU"
---
+++ API Activities/APIs Day 03 Activities (1)/10-Stu_BankDeserts_Heatmap/Solved/config.py
@@ -0,0 +1,4 @@
+# Enter your API keys
+gkey = "AIzaSyCu1ZA_-wimdbPaUssBz2Y3IjJQ_3FEaGU"
+
+census_key = "85ac64b6b5a9c0901b00329d1ef41f0c53ccfc98"
